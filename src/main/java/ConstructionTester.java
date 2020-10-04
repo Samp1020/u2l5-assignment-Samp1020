3c26d1390ea0d05c484aab0585d50423d9ff75a7
Samantha Padilla <samantha.227478@nv.ccsd.net>
2020-10-04 21:21:22 +0000

    added the ConstructionTester code

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,31 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        //variables
+        double tRate;
+        int nBoards;
+        int nWindows;
+
+        //scanner
+        Scanner scan = new Scanner(System.in);
+
+        //user input
+        System.out.println("Enter the sales tax rate: ");
+        tRate = scan.nextDouble();
+        System.out.print("How many boards do you need? ");
+        nBoards = scan.nextInt();
+        System.out.println("How many windows do you need? ");
+        nWindows = scan.nextInt();
+
+        //the math behind the code 
+        Construction theConstruction = new Construction(5.0, 11.0, tRate);
+        double theTotal = theConstruction.lumberCost(nBoards) + theConstruction.windowCost(nWindows);
+        double grandTotal = theConstruction.grandTotal(theTotal);
+        System.out.println("Total: " + theTotal);
+        System.out.println("The grand total is " + grandTotal);
+
+
+
+
     }
 }
